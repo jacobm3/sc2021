0a60541def830a0125a4eff76aa5c9e3ffd44ca3
Jacob <jacobm3@gmail.com>
2021-02-01 20:30:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,28 @@
-provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables described above, so that each user can have
-  # separate credentials set in the environment.
-  #
+provider "google" {
+  project     = "sc2021"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+  access_token = data.vault_generic_secret.gcp_token.data.token
 }
 
+# Dynamic GCP access token for provisioning resources
 data "vault_generic_secret" "gcp_token" {
-  #path = "gcp/token/my-token-roleset"
-  path = "kv/data/secret"
+  path = "gcp/token/my-token-roleset"
 }
 
+resource "google_storage_bucket" "static-site" {
+  name          = "sc2021-bucket"
+  location      = "US"
+  force_destroy = true
 
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+#output "gcp_access_token" {
+#  value = data.vault_generic_secret.gcp_token.data.token
+#}
